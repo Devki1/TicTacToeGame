fe02162fc45574e0b8bc0dc657698890bd22c12b
Devki1 <gdev3195@gmail.com>
2020-02-29 18:07:12 +0530

    ADDED:Start resetting the board

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,2 +1,14 @@
 #!/bin/bash -x
+declare -a board
 echo "Welcome to Tic Tac Toe Game"
+board=(" " " " " " " " " " " " " " " " " ")
+function resultingboard()
+{
+	echo "${board[0]} | ${board[1]} | ${board[2]}"
+	echo "----------"
+	echo "${board[3]} | ${board[4]} | ${board[5]}"
+	echo "----------"
+	echo "${board[6]} | ${board[7]} | ${board[8]}"
+}
+#Call the function 
+resultingboard

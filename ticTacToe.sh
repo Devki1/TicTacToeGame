bbe1019515b95130f0908b8e6cc9188526eacd54
Devki1 <gdev3195@gmail.com>
2020-03-03 14:54:19 +0530

    ADDED:To play a computer

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -2,11 +2,11 @@
 declare -a board
 echo "Welcome to Tic Tac Toe Game"
 board=(" " " " " " " " " " " " " " " " " ")
-Dot=0
-cross=1
-User=0
-Computer=1
-function resultingboard()
+DOT=0
+CROSS=1
+USER=0
+COMPUTER=1
+function resultingBoard()
 {
 	echo "${board[0]} | ${board[1]} | ${board[2]}"
 	echo "----------"
@@ -22,53 +22,53 @@ function playerTurn()
 	read data
 	if(($data>=0 && $data<=8))
 	then
-		if [[ "${board[data]}"!=X || "${board[data]}"!=O ]]
+		if [[ "${board[data]}"!=X && "${board[data]}"!=O ]]
 		then
 			board[$data]="$playerLetter"
+			resultingBoard
 		else
-			valid $playerLetter 
+			playerTurn $playerLetter 
 		fi
 	fi
-resultingboard
 }
 #Logic is used  for winning condition
-function winningCondition()
+function checkWin()
 {
 	letter=$1
-	if [ "${board[0]} ${board[1]} ${board[2]}" = "$letter$letter$letter" ]
+	if [[ "${board[0]} ${board[1]} ${board[2]}" == "$letter $letter $letter" ]]
 	then
-		result="$win"
-	elif [ "${board[3]} ${board[4]} ${board[5]}" = "$letter$letter$letter" ]
+		result="win"
+	elif [[ "${board[3]} ${board[4]} ${board[5]}" == "$letter $letter $letter" ]]
 	then
-		result="$win"
-	elif [ "${board[6]} ${board[7]} ${board[8]}" = "$letter$letter$letter" ]
+		result="win"
+	elif [[ "${board[6]} ${board[7]} ${board[8]}" == "$letter $letter $letter" ]]
 	then
-		result="$win"
-	elif [ "${board[0]} ${board[3]} ${board[6]}" = "$letter$letter$letter" ]
+		result="win"
+	elif [[ "${board[0]} ${board[3]} ${board[6]}" == "$letter $letter $letter" ]]
 	then
-		result="$win"
-	elif [ "${board[1]} ${board[4]} ${board[7]}" = "$letter$letter$letter" ]
+		result="win"
+	elif [[ "${board[1]} ${board[4]} ${board[7]}" == "$letter $letter $letter" ]]
 	then
-		result="$win"
-	elif [ "${board[2]} ${board[5]} ${board[8]}" = "$letter$letter$letter" ]
+		result="win"
+	elif [[ "${board[2]} ${board[5]} ${board[8]}" == "$letter $letter $letter" ]]
 	then
-		result="$win"
-	elif [ "${board[6]} ${board[4]} ${board[8]}" = "$letter$letter$letter" ]
+		result="win"
+	elif [[ "${board[0]} ${board[4]} ${board[8]}" == "$letter $letter $letter" ]]
 	then
-		result="$win"
-	elif [ "${board[2]} ${board[4]} ${board[6]}" = "$letter$letter$letter" ]
+		result="win"
+	elif [[ "${board[2]} ${board[4]} ${board[6]}" == "$letter $letter $letter" ]]
 	then
-		result="$win"
+		result="win"
 	else
 		flag=0
 		for((count=0;count<${#board[@]};count++))
 		do
-			if [[ "${board[$count]}"!=X || "${board[$count]}"!=O ]]
+			if [ "${board[$count]}" != "X" ] && [ "${board[$count]}" != "O" ]
 			then
 				flag=1
 			fi
 		done
-		if [ $flag==0 ]
+		if(($flag==0))
 		then
 			result="draw"
 		else
@@ -77,32 +77,78 @@ function winningCondition()
 	fi
 	echo $result
 }
+#Computer play turn
+function computerTurn()
+{
+	computerLetter=$1
+
+	data=$((RANDOM%9))
+	if [ "${board[$data]}" != "X" ] && [ "${board[$data]}" != "O" ]
+	then
+		echo "Computer turn"
+		board[$data]="$computerLetter"
+		resultingBoard
+	else
+		computerTurn $computerLetter
+	fi
+}
+#Assigned a letter
 function getLetter()
 {
 	checkLetter=$((RANDOM%2))
 	case $checkLetter in
-		$Dot)
+		$DOT)
 			playerLetter="O"
-			;;
-		$cross)
+			computerLetter="X"
+		;;
+		$CROSS)
 			playerLetter="X"
-			;;
+			computerLetter="O"
+		;;
 	esac
-	echo $playerLetter
+	#echo "Player: $playerLetter Computer: $computerLetter"
 }
+#Player chance
 function whoPlayFirst()
 {
 	playerChance=$((RANDOM%2))
 	case $playerChance in
-		$User)
-			echo "User chance play first: "
-         ;;
-		$Computer)
-			echo "computer chance play first:"
-			;;
+		$USER)
+			 echo "User"
+		;;
+		$COMPUTER)
+			 echo "Computer"
+		;;
 	esac
 }
-winningCondition $playerLetter
-whoPlayFirst
-letter="$(getLetter)"
-playerTurn $letter
+resultingBoard
+getLetter
+chance="$(whoPlayFirst)"
+#checkWin $playerLetter
+flag=0
+if [ $chance == "Computer" ]
+then
+	flag=1
+fi
+while((0==0))
+do
+	if (( $flag%2==0 ))
+	then
+		computerTurn $computerLetter
+		result="$(checkWin $computerLetter)"
+		if [[ $result == "win" || $result == "draw" ]]
+		then
+			printf "Computer $result\n"
+			break
+		fi
+	else
+		playerTurn $playerLetter
+		result="$(checkWin $playerLetter)"
+		if [[ $result == "win" || $result == "draw" ]]
+		then
+			printf "Player $result\n"
+			break
+		fi
+	fi
+	flag=$((flag+1))
+done
